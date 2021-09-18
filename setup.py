23ea5d7ddcd4ec14259202538108444b99e5966a
Antonio Nesic <nesic.antonio@gmail.com>
2021-09-18 17:39:52 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crometeo",
+    version="0.0.1",
+    author="Antonio Nesic",
+    author_email="nesic.antonio@gmail.com",
+    description="Unofficial reader for Meteo.hr",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mutantkeyboard/crometeo",
+    project_urls={
+        "Bug Tracker": "https://github.com/mutantkeyboard/crometeo/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: End Users/Desktop",
+        "Framework :: Scrapy",
+        "Topic :: Internet :: WWW/HTTP"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
